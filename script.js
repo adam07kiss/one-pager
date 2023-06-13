d17b6e6baec97c40af1c552db77f1d8846bb1b87
Ádám Kiss <adam08kiss@gmailcom>
2023-06-13 09:17:14 +0200

    Fixed breakpoints

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const toggle = () => {
     const nav = document.getElementById("navbar");
-    const header  = document.getElementById("headerToggle");
-    nav.className === "navbar" ? nav.className += " responsive" : nav.className = "navbar";
-    header.className === "headerToggle" ? header.className += " responsive" : header.className = "headerToggle";
+    const header = document.getElementById("header-toggle");
 
     console.log("done")
+    if (nav.className === "navbar") {
+        $(nav).addClass("responsive")
+    } else {
+        $(nav).removeClass("responsive")
+    }
+
+    if (header.className === "header-toggle") {
+        $(header).addClass("responsive")
+    } else {
+        $(header).removeClass("responsive")
+    }
 };
